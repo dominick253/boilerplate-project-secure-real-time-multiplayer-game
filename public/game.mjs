99a2b0996adfdbc0b32e0c44e73338101931d140
Replit user <>
2023-07-07 10:11:00 -0500

    Inital 2

--- public/game.mjs
+++ public/game.mjs
@@ -4,3 +4,191 @@ import Collectible from './Collectible.mjs';
 const socket = io();
 const canvas = document.getElementById('game-window');
 const context = canvas.getContext('2d');
+
+const players = [];
+const collectibles = [];
+
+window.onload = function () {
+  const canvas = document.getElementById('game-window');
+  const ctx = canvas.getContext('2d');
+  ctx.fillStyle = 'black';
+  ctx.fillRect(20, 20, 150, 100);
+};
+
+document.addEventListener('keydown', handlePlayerMovement);
+
+function handlePlayerMovement(event) {
+  const player = players.find((p) => p.id === socket.id);
+  if (!player) return;
+
+  const movementMap = {
+    w: { direction: 'up', pixels: 10 },
+    s: { direction: 'down', pixels: 10 },
+    a: { direction: 'left', pixels: 10 },
+    d: { direction: 'right', pixels: 10 },
+  };
+
+  const movement = movementMap[event.key];
+  if (!movement) return;
+
+  movePlayer(player, movement.direction, movement.pixels);
+}
+
+function movePlayer(player, direction, pixels) {
+  switch (direction) {
+    case 'up':
+      player.y -= pixels;
+      break;
+    case 'down':
+      player.y += pixels;
+      break;
+    case 'left':
+      player.x -= pixels;
+      break;
+    case 'right':
+      player.x += pixels;
+      break;
+    default:
+      break;
+  }
+}
+
+function checkCollisions() {
+  const player = players.find((p) => p.id === socket.id);
+  if (!player) return;
+
+  const collidedItem = collectibles.find((item) => collision(player, item));
+  if (collidedItem) {
+    player.score += collidedItem.value;
+    removeCollectible(collidedItem);
+  }
+}
+
+
+function collision(player, item) {
+  return player.collision(item);
+}
+
+function removeCollectible(collectible) {
+  const index = collectibles.indexOf(collectible);
+  if (index !== -1) {
+    collectibles.splice(index, 1);
+    const { x, y } = generateRandomCoordinates(); // Generate new random coordinates
+    addCollectible(); // Add new collectible with new coordinates
+  }
+}
+
+
+function generateRandomCoordinates() {
+  const maxX = canvas.width - 10;
+  const maxY = canvas.height - 10;
+  const x = Math.random() * maxX;
+  const y = Math.random() * maxY;
+  return { x, y };
+}
+
+function calculateRank(playersArray) {
+  const sortedPlayers = [...playersArray].sort((a, b) => b.score - a.score);
+  const currentPlayerIndex = sortedPlayers.findIndex((p) => p.id === socket.id) + 1;
+  return `Rank: ${currentPlayerIndex}/${sortedPlayers.length}`;
+}
+
+function addPlayer(id, x, y) {
+  const player = new Player(id, x, y);
+  players.push(player);
+}
+
+function removePlayer(id) {
+  const index = players.findIndex((p) => p.id === id);
+  if (index !== -1) {
+    players.splice(index, 1);
+  }
+}
+
+function generateRandomId() {
+  return Math.random().toString(36).substr(2, 9);
+}
+
+
+function addCollectible() {
+  const id = generateRandomId(); // Generate a random ID
+  const x = Math.random() * (canvas.width - 10);
+  const y = Math.random() * (canvas.height - 10);
+  const value = 10;
+  const collectible = new Collectible(id, value, x, y);
+  collectibles.push(collectible);
+}
+
+
+
+function updateGameState() {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  players.forEach((player) => {
+    context.fillStyle = 'blue';
+    context.fillRect(player.x, player.y, 20, 20);
+    context.fillStyle = 'black';
+    context.fillText(`Score: ${player.score}`, player.x, player.y - 10);
+  });
+
+  collectibles.forEach((collectible) => {
+    context.fillStyle = 'green';
+    context.fillRect(collectible.x, collectible.y, 10, 10);
+  });
+
+  const playerRank = calculateRank(players);
+  const rankElement = document.getElementById('player-rank');
+  rankElement.textContent = playerRank;
+
+  checkCollisions();
+}
+
+function gameLoop() {
+  updateGameState();
+  requestAnimationFrame(gameLoop);
+}
+
+socket.on('connect', () => {
+  const { id } = socket;
+  const startX = canvas.width / 2;
+  const startY = canvas.height / 2;
+  addPlayer(id, startX, startY);
+
+  
+  
+  addCollectible();
+});
+
+socket.on('disconnect', () => {
+  const { id } = socket;
+  removePlayer(id);
+});
+
+socket.on('playerMovement', ({ direction, pixels }) => {
+  const player = players.find((p) => p.id === socket.id);
+  if (player) {
+    movePlayer(player, direction, pixels);
+  }
+});
+
+socket.on('playerDisconnect', ({ id }) => {
+  removePlayer(id);
+});
+
+socket.on('collectibleAdded', ({ id, value }) => {
+  const { x, y } = generateRandomCoordinates(); // Generate random coordinates once
+  console.log(x + y)
+  addCollectible();
+  updateGameState();
+});
+
+
+
+socket.on('collectibleRemoved', ({ id }) => {
+  const collectible = collectibles.find((c) => c.id === id);
+  if (collectible) {
+    removeCollectible(collectible);
+  }
+});
+
+gameLoop();
--- server.js
+++ server.js
@@ -4,11 +4,53 @@ const bodyParser = require('body-parser');
 const expect = require('chai');
 const socket = require('socket.io');
 const cors = require('cors');
+const helmet = require('helmet');
 
 const fccTestingRoutes = require('./routes/fcctesting.js');
 const runner = require('./test-runner.js');
-
 const app = express();
+
+const server = require('http').Server(app);
+const io = socket(server);  // Create a Socket.IO server
+
+io.on('connection', function (socket) {
+  console.log('A user connected');
+
+  socket.on('disconnect', function () {
+    console.log('A user disconnected');
+  });
+});
+
+app.use((req, res, next) => {
+  res.setHeader('Surrogate-Control', 'no-store');
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  next();
+});
+
+app.use(
+  helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        styleSrc: ["'self'", 'maxcdn.bootstrapcdn.com', 'https://fonts.googleapis.com', "'unsafe-inline'"],
+        scriptSrc: ["'self'", 'cdnjs.cloudflare.com', "'unsafe-inline'"],
+        fontSrc: ["'self'", 'https://fonts.gstatic.com'],  // Add this line
+      },
+    },
+    dnsPrefetchControl: false,
+    expectCt: { maxAge: 0 },
+    frameguard: { action: 'sameorigin' },
+    hidePoweredBy: { setTo: 'PHP 7.4.3' },
+    hsts: { maxAge: 0 },
+    ieNoOpen: true,
+    noSniff: true,
+    permittedCrossDomainPolicies: true,
+    referrerPolicy: { policy: 'no-referrer' },
+    xssFilter: true,
+  })
+);
 
 app.use('/public', express.static(process.cwd() + '/public'));
 app.use('/assets', express.static(process.cwd() + '/assets'));
@@ -17,17 +59,17 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 //For FCC testing purposes and enables user to connect from outside the hosting platform
-app.use(cors({origin: '*'})); 
+app.use(cors({ origin: '*' }));
 
 // Index page (static HTML)
 app.route('/')
-  .get(function (req, res) {
+  .get(function(req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
-  }); 
+  });
 
 //For FCC testing purposes
 fccTestingRoutes(app);
-    
+
 // 404 Not Found Middleware
 app.use(function(req, res, next) {
   res.status(404)
@@ -38,11 +80,11 @@ app.use(function(req, res, next) {
 const portNum = process.env.PORT || 3000;
 
 // Set up server and tests
-const server = app.listen(portNum, () => {
+ server.listen(portNum, () => {
   console.log(`Listening on port ${portNum}`);
-  if (process.env.NODE_ENV==='test') {
+  if (process.env.NODE_ENV === 'test') {
     console.log('Running Tests...');
-    setTimeout(function () {
+    setTimeout(function() {
       try {
         runner.run();
       } catch (error) {
